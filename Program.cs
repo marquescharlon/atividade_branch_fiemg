b1423936e15bdedd8fc69249bab977e876aea6b0
LucasFernandes07 <lc.martins031@gmail.com>
2024-09-30 14:11:48 -0300

    Codigo atualizado

--- Program.cs
+++ Program.cs
@@ -1,71 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿﻿using System.IO;
 
-class Program
+Console.Clear();
+
+int largura = 50; // Ajuste a largura do cabeçalho conforme necessário
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("=".PadLeft(37, '='));
+Console.WriteLine("Atividade 13 - Jogo da Mega-Sena");
+Console.WriteLine("=".PadLeft(37, '='));
+Console.ResetColor();
+
+
+Random random = new Random();
+
+int qtdDezena
+    qtdDezenaInformada;
+
+decimal valorPremio;
+
+bool repetir = "";
+
+Console.Write("Deseja realizar quantos jogos: ");
+if (int.Parse(Console.ReadLine(), out qtdJogoInformada))
 {
-    static void Main()
+    do
     {
-        Console.WriteLine("Bem-vindo ao gerador de jogos de Mega-Sena!");
-
-        // Fase 2: Solicitar a quantidade de jogos
-        Console.Write("Informe a quantidade de jogos que deseja fazer: ");
-        int quantidadeJogos = int.Parse(Console.ReadLine());
-
-        // Fase 3: Solicitar a quantidade de dezenas
-        int quantidadeDezenas;
-        do
+        Console.Write("Informar a quantidade de dezena: ");
+        if (int.Parse(Console.ReadLine(), out qtdDezenaInformada))
         {
-            Console.Write("Informe a quantidade de dezenas (6 a 15): ");
-            quantidadeDezenas = int.Parse(Console.ReadLine());
-        } while (quantidadeDezenas < 6 || quantidadeDezenas > 15);
+            if (qtdDezenaInformada < 6 || qtdDezenaInformada > 15)
+                    //repetir = true;
+                else
+                        repetir = false;
 
-        // Gerar os jogos
-        List<string> jogos = new List<string>();
-        Random random = new Random();
-        
-        for (int i = 0; i < quantidadeJogos; i++)
-        {
-            HashSet<int> dezenas = new HashSet<int>();
-            while (dezenas.Count < quantidadeDezenas)
+            if (repetir == false)
             {
-                dezenas.Add(random.Next(1, 61)); // Números entre 1 e 60
+                Console.WriteLine();
+                for (qtdJogo = 1; qtdJogoInformada <= qtdJogoInformada; qtdJogo--)
+                {
+                    for (qtdDezena = 1; qtdDezena <= qtdDezenaInformada)
+                    {
+
+                    }
+                    escrever.WriteLine();
+                }
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Jogos gerados e salvos no arquivo 'jogos-mega-sena.txt'.\n");
+                Console.ResetColor();
             }
-            List<int> dezenasOrdenadas = new List<int>(dezenas);
-            dezenasOrdenadas.Sort();
-            jogos.Add(string.Join("-", dezenasOrdenadas));
-        }
-
-        // Exibir os jogos
-        Console.WriteLine("\nJogos gerados:");
-        foreach (var jogo in jogos)
-        {
-            Console.WriteLine(jogo);
-        }
-
-        // Fase 5: Salvar o jogo em um arquivo .TXT
-        using (StreamWriter escrever = new StreamWriter("jogos-mega-sena.txt"))
-        {
-            foreach (var jogo in jogos)
+            else
             {
-                escrever.WriteLine(jogo);
+                Console.WriteLine("Quantidade dezena menor que 6");
+                repetir = true;
+
+            }
+        else
+            {
+                repetir = true;
+                Console.WriteLine("Número inválido!");
             }
         }
-
-        Console.WriteLine("\nJogos salvos em 'jogos-mega-sena.txt'.");
-
-        // Fase 6: Solicitar o valor do prêmio
-        Console.Write("Informe o valor do prêmio: ");
-        decimal premio = decimal.Parse(Console.ReadLine());
-
-        // Fase 7: Calcular a distribuição do prêmio
-        decimal premioSeisDezenas = premio * 0.75m;
-        decimal premioCincoDezenas = premio * 0.15m;
-        decimal premioQuatroDezenas = premio * 0.10m;
-
-        Console.WriteLine($"\nDistribuição do prêmio:");
-        Console.WriteLine($"75% para quem acertar 6 dezenas: {premioSeisDezenas:C}");
-        Console.WriteLine($"15% para quem acertar 5 dezenas: {premioCincoDezenas:C}");
-        Console.WriteLine($"10% para quem acertar 4 dezenas: {premioQuatroDezenas:C}");
+        //while (repetir == true);
     }
+else
+
+        Console.WriteLine("Número inválido!");
 }
+
+// Solicitar o valor do prêmio
+
+Console.Write("Informe o valor do prêmio: ");
+if (decimal.TryParse(Console.ReadLine(), out valorPremio))
+{
--- obj/Debug/net8.0/MegaSena.AssemblyInfo.cs
+++ obj/Debug/net8.0/MegaSena.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MegaSena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13faf0157ed1f82bd226571c550024685497fc1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d17703eb7df2f5a3d4af4ac5d39f0dcd0479fd9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MegaSena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MegaSena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

b0663560daba197362ea11a28341ab1da5e17840
Pedro Queiroga <heringerleonardo560@gmail.com>
2024-09-30 08:31:55 -0300

    ead

--- obj/Debug/net8.0/MegaSena.AssemblyInfo.cs
+++ obj/Debug/net8.0/MegaSena.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MegaSena")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13faf0157ed1f82bd226571c550024685497fc1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62366ff0470facb7f4673163b3e221d161a6669")]
 [assembly: System.Reflection.AssemblyProductAttribute("MegaSena")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MegaSena")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
